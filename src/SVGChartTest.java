69d59b0568a2ba8a544bddaaf63eba044314d187
Robin Smidsrød <robin@smidsrod.no>
2010-04-25 21:03:55 +0200

    Added generator code for BarChart

--- src/SVGChartTest.java
+++ src/SVGChartTest.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Random;
 
+import no.smidsrod.robin.svg.library.BarChart;
 import no.smidsrod.robin.svg.library.Item;
 import no.smidsrod.robin.svg.library.SVGRenderer;
 import no.smidsrod.robin.svg.library.SectorChart;
@@ -15,33 +16,72 @@ public class SVGChartTest {
 	 * @throws FileNotFoundException 
 	 */
 	public static void main(String[] args) throws FileNotFoundException {
+		createSectorChart();
+		createBarChart();
+	}
+
+	private static void createSectorChart() throws FileNotFoundException {
 		// Create new sector chart
-		SectorChart chart = new SectorChart("My cool chart", "With an even cooler description");
+		SectorChart chart = new SectorChart("My cool sector chart", "With an even cooler description");
 		
+		chart.getRange(0).setUnit("percentage");
+		
+		// Fetch an empty list of Items
 		List<Item> items = chart.getItemList();
 		
+		// We need some random numbers...
 		Random random = new Random();
-		// Put in some items we can use
-		for (int i=1; i < 10; i++) {
-			Item person = new Item("Person " + i);
-			items.add(person);
-			Value value = new Value(chart);
-			value.set(0, random.nextDouble());
-			person.getValueList().add(value);
+		
+		// Put some Items into the list
+		for (int i=1; i <= 10; i++) {
+			Item item = new Item("Person " + i);
+			if ( i % 2 == 0) {
+				item.setHighlighted(true);
+			}
+			items.add(item);
+			
+			Value value = new Value(chart.getDimensionCount());
+			value.set(chart.getRange(0).getDimension(), random.nextDouble() * 10);
+			
+			item.getValueList().add(value);
 		}
 		
-		// Output indented SVG to standard output
+		// Output indented SVG to file
 		SVGRenderer r = chart.getSVGRenderer();
 		r.setPrettyPrint(true);
-		r.renderSVG(System.out);
+		r.storeSVGDocument(new File("C:\\Temp\\sector_chart.svg"));
+	}
+
+	private static void createBarChart() throws FileNotFoundException {
+		// Create new sector chart
+		BarChart chart = new BarChart("My cool bar chart", "With an even cooler description");
 		
-		// Output compact SVG to file
-		r.setPrettyPrint(true);
-		r.storeSVG(new File("C:\\Temp\\test.svg"));
+		chart.getRange(0).setUnit("km");
 		
-		for(Item i: items) {
-			System.err.println(i.getColor().toString());
+		// Fetch an empty list of Items
+		List<Item> items = chart.getItemList();
+		
+		// We need some random numbers...
+		Random random = new Random();
+		
+		// Put some Items into the list
+		for (int i=1; i <= 5; i++) {
+			Item item = new Item("Person " + i);
+			if ( i % 2 == 0) {
+				item.setHighlighted(true);
+			}
+			items.add(item);
+			
+			Value value = new Value(chart.getDimensionCount());
+			value.set(chart.getRange(0).getDimension(), random.nextDouble() * 10);
+			
+			item.getValueList().add(value);
 		}
+		
+		// Output indented SVG to file
+		SVGRenderer r = chart.getSVGRenderer();
+		r.setPrettyPrint(true);
+		r.storeSVGDocument(new File("C:\\Temp\\bar_chart.svg"));
 	}
 
 }
